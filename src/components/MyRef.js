e9450342a5cfff4e3752bd8508ab9b9732764c2b
Hassaan Habib <hassaan.habib@empglabs.com>
2021-08-13 13:00:41 +0500

    Added ref practice

---
+++ src/components/MyRef.js
@@ -0,0 +1,24 @@
+import React,{useState,forwardRef,useRef,useImperativeHandle} from 'react'
+
+const MyRef = React.forwardRef((props,ref) => {
+    const [count,setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(count+1);
+    }
+   useImperativeHandle(
+        ref,
+        () => {
+            return {incrementCount: ()=>{
+                incrementCount();
+            }}
+        }
+    )
+    return (
+        <div>
+            <h2>My ref with count: {count}</h2>
+        </div>
+    )
+})
+export default MyRef
+ 
--- src/pages/Refs.js
+++ src/pages/Refs.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import MyRef from '../components/MyRef';
+
 
 function Refs() {
+    const myRef = useRef(null);
+    const incrementCount = () =>{
+        myRef.current.incrementCount();
+    }
     return (
-        <div className="Refs">
-            <h2>Practicing Refs</h2>
+        <div className="Refs" align="center">
+            {/* <h2>Practicing Refs</h2> */}
+            <MyRef ref={myRef}/>
+            ~
+            <button onClick={incrementCount}>Increment the count</button>
         </div>
     )
 }
